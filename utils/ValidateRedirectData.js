d419241cf07c2d8f1aa8efc54aa1a1bc208e2f7c
Armaan Gupta <guppythegod@tcff.co>
2020-04-26 12:49:11 -0500

    added iris logger for ValidateRedirectData middleware

--- utils/ValidateRedirectData.js
+++ utils/ValidateRedirectData.js
@@ -1,10 +1,12 @@
+const IRIS = require("../config/iris");
+
 const validateRedirectData = (req, res, next) => {
     const appNameRaw = req.query.appname;
     const callback = req.query.callback;
     const aidn = req.query.aidn;
     
     if ([appNameRaw, callback, aidn].some((val) => val === undefined)) {
-        // log to iris
+        IRIS.warn("redirect data was invalidated",{appNameRaw,callback,aidn},["api"]);
         res.redirect("/404");
     }
     else {
