dd77c73c51b9541b78bf42be050f3d4d3f1ebb98
Armaan Gupta <guppythegod@tcff.co>
2020-04-26 12:51:35 -0500

    added iris logger for verify key

--- utils/VerifyKey.js
+++ utils/VerifyKey.js
@@ -1,5 +1,6 @@
 const Keychain = require('../models/KeychainModel');
 const jwt = require('jsonwebtoken');
+const IRIS = require("../config/iris");
 
 const rawSecret = process.env.KEYCHAIN_SECURITY_CODE;
 const SECRET = Buffer.from(rawSecret, 'base64');
@@ -8,28 +9,28 @@ const verifyKey = (key, ksn, aidn) => {
     return new Promise((resolve, reject) => {
         jwt.verify(key, SECRET, (err, payload) => {
             if (err) {
-                console.log(err); // log to iris
+                IRIS.info("key invalid",{ksn,aidn,key},["api"]);
                 reject({status:401, code:"invalid_key"});
             }
 
             if (payload.ksn !== ksn) {
-                console.log("ksn_mismatch"); //log to iris
+                IRIS.info("ksn mismatch",{ksn,aidn,key},["api"]);
                 reject({status:401, code:"ksn_mismatch"});
             }
 
             if (payload.aidn !== aidn) {
-                console.log("aidn_mismatch"); // log to iris
+                IRIS.info("aidn mismatch",{ksn,aidn,key},["api"]);
                 reject({status:401, code:"aidn_mismatch"});
             }
 
             Keychain.findOne({where: {key}})
             .catch(err => {
-                console.log(err); // log to iris
+                IRIS.critical("Keychain.findOne failed",{key,err},["api","ise"]);
                 reject({status:500, code:"internal_server_error"});
             })
             .then(keychain => {
                 if (keychain.expired) {
-                    console.log("expired_keychain"); // log to iris
+                    IRIS.info("keychain was expired",{key},["api"]);
                     reject({status:401, code:"expired_keychain"});
                 }
                 
