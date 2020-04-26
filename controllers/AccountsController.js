7678476a658c4221a29927545ece4566deecd954
Armaan Gupta <guppythegod@tcff.co>
2020-04-26 12:37:12 -0500

    Removed hashtags from tags, added Iris log statements for Appchains controller

--- controllers/AccountsController.js
+++ controllers/AccountsController.js
@@ -12,25 +12,25 @@ const IRIS = require("../config/iris");
 
 
 exports.getLoginPage = (req, res) => {
-    IRIS.info("user routed to login page",{url: req.originalUrl},["#ui"]);
+    IRIS.info("user routed to login page",{url: req.originalUrl},["ui"]);
     res.render('login', {appName: res.locals.appName});
 };
 
 
 exports.getSignupPage = (req, res) => {
-    IRIS.info("user routed to signup page",{url: req.originalUrl},["#ui"]);
+    IRIS.info("user routed to signup page",{url: req.originalUrl},["ui"]);
     res.render('signup', {appName: res.locals.appName});
 };
 
 
 exports.getResetPasswordPage = (req, res) => {
-    IRIS.info("user routed to reset page",{url: req.originalUrl},["#ui"]);
+    IRIS.info("user routed to reset page",{url: req.originalUrl},["ui"]);
     res.render('resetPassword', {appName: res.locals.appName});
 };
 
 
 exports.getFindUsernamePage = (req, res) => {
-    IRIS.info("user routed to find username page",{url: req.originalUrl},["#ui"]);
+    IRIS.info("user routed to find username page",{url: req.originalUrl},["ui"]);
     res.render('findUsername', {appName: res.locals.appName});
 };
 
@@ -41,20 +41,20 @@ exports.verifyKey = (req, res) => {
     const ksn = req.body.ksn;
     const aidn = req.body.aidn;
 
-    IRIS.info("key verification started",{key,ksn,aidn},["#api"]);
+    IRIS.info("key verification started",{key,ksn,aidn},["api"]);
 
     verifyKey(key, ksn, aidn)
     .then(info => {
         const ccn = info.ccn;
 
-        IRIS.info("key verified",{ccn},["#api","#success"]);
+        IRIS.info("key verified",{ccn},["api","success"]);
         res.json({status:202, data:{ccn,ksn,aidn}});
     })
     .catch(err => {
         const status = err.status;
         const errorCode = err.code;
 
-        IRIS.info("failed to verify key",{status, errorCode},["#api"]);
+        IRIS.info("failed to verify key",{status, errorCode},["api"]);
         res.json({status, data:{errorCode}});
     })
 };
@@ -73,7 +73,7 @@ exports.signup = (req, res) => {
     const salt = bcrypt.genSaltSync(12);
     const bpassword = bcrypt.hashSync(password, salt);
 
-    IRIS.info("signup started",{aidn,email,username},["#api"]);
+    IRIS.info("signup started",{aidn,email,username},["api"]);
 
     generateKSN(ksn => {
         Account.create({
@@ -90,18 +90,18 @@ exports.signup = (req, res) => {
         .catch(err => {
             console.log(err)
             if (err.name === "SequelizeUniqueConstraintError") {
-                IRIS.info("signup failed",{errorCode: "email_or_phone_inuse"},["#api"]);
+                IRIS.info("signup failed",{errorCode: "email_or_phone_inuse"},["api"]);
                 res.json({status: 401, data: {errorCode: "email_or_phone_inuse"}});
             }
             else {
-                IRIS.critical("ISE during signup",{err},["#api","#catch"]);
+                IRIS.critical("ISE during signup",{err},["api","catch"]);
                 res.json({status: 500, data: err});
             }
         })
         .then(account => {
             generateKeychain(account.ksn, aidn)
             .catch(err => {
-                IRIS.critical("signup: generate keychain failed",{err},["#api","#catch","#ise"]);
+                IRIS.critical("signup: generate keychain failed",{err},["api","catch","ise"]);
                 res.json({status: 500, data: err})
             })
             .then(key => {
@@ -109,10 +109,10 @@ exports.signup = (req, res) => {
                     postage.sendWelcomeEmail(account.fname, account.email);
                 }
                 catch (err) {
-                    IRIS.error("sending welcome email failed", {err}, ["#api","#catch","#ise"]);
+                    IRIS.error("sending welcome email failed", {err}, ["api","catch","ise"]);
                 }
                 finally {
-                    IRIS.info("account created",{ksn:account.ksn},["#api","#success"]);
+                    IRIS.info("account created",{ksn:account.ksn},["api","success"]);
                     res.json({status: 202, data: {key}});
                 }
             });
@@ -129,25 +129,25 @@ exports.login = (req, res) => {
     const password = req.body.password;
     const aidn = req.body.AIDN;
 
-    IRIS.info("login initiated",{username,aidn},["#api"]);
+    IRIS.info("login initiated",{username,aidn},["api"]);
 
     Account.findOne({where: {username}})
     .then(account => {
         if (account === null) {
-            IRIS.info("account not found",{username},["#api"]);
+            IRIS.info("account not found",{username},["api"]);
             res.json({status: 404, data:{errorCode:"no_account_found"}});
         }
         else {
             bcrypt.compare(password, account.bpassword)
             .catch(err => {
-                IRIS.critical("bcrypt.compare failed",{err},["#api","#ise"]);
+                IRIS.critical("bcrypt.compare failed",{err},["api","ise"]);
                 res.json({status: 500, data: err});
             })
             .then(result => {
                 if (result) {
                     generateKeychain(account.ksn, aidn)
                     .catch(err => {
-                        IRIS.critical("login: keychain generation failed",{err},["#api","#ise"]);
+                        IRIS.critical("login: keychain generation failed",{err},["api","ise"]);
                         res.json({status: 500, data: err});
                     })
                     .then(key => {
@@ -155,16 +155,16 @@ exports.login = (req, res) => {
                             postage.sendLoginNotificationEmail(account.fname, account.email, {});
                         }
                         catch (err) {
-                            IRIS.error("login notification email failed to send",{ksn:account.ksn},["#api","#catch"]);
+                            IRIS.error("login notification email failed to send",{ksn:account.ksn},["api","catch"]);
                         }
                         finally {
-                            IRIS.info("login succeeded",{ksn:account.ksn},["#api","success"]);
+                            IRIS.info("login succeeded",{ksn:account.ksn},["api","success"]);
                             res.json({status: 202, data: {key}});
                         }
                     });
                 }
                 else {
-                    IRIS.info("user entered incorrect password",{ksn:account.ksn},["#api","#caught"]);
+                    IRIS.info("user entered incorrect password",{ksn:account.ksn},["api","caught"]);
                     res.json({status: 406, data:{errorCode:"incorrect_password"}});
                 }
             });
@@ -176,15 +176,15 @@ exports.login = (req, res) => {
 exports.logout = (req, res) => {
     const ccn = res.locals.ccn;
 
-    IRIS.info("logout started",{ccn},["#api"]);
+    IRIS.info("logout started",{ccn},["api"]);
 
     Keychain.update({expired: true},{where: {ccn}})
     .catch(err => {
-        IRIS.critical("logging user out failed",{err},["#api","#ise"]);
+        IRIS.critical("logging user out failed",{err},["api","ise"]);
         res.json({status: 500, data:{errorCode:"internal_server_error"}});
     })
     .then(update => {
-        IRIS.info("user logged out",{update},["#api","#success"]);
+        IRIS.info("user logged out",{update},["api","success"]);
         res.json({status: 202});
     });
 };
@@ -194,25 +194,25 @@ exports.requestPasswordResetCode = (req, res) => {
     const username = req.body.username;
     const deliveryType = req.body.delivery_type;
 
-    IRIS.info("requesting password reset code",{username},["#api"]);
+    IRIS.info("requesting password reset code",{username},["api"]);
 
     Account.findOne({where: {username}})
     .then(account => {
         const ksn = account.ksn;
 
         if (account === null) {
-            IRIS.warn("no account found when requesting for password reset code",{username},["#api"]);
+            IRIS.warn("no account found when requesting for password reset code",{username},["api"]);
             res.json({status: 404, data: {errorCode: "no_account_found"}});
         }
         else {
             createResetCode(account.ksn)
             .catch(err => {
-                IRIS.critical("creating reset code failed",{err,ksn},["#api","#ise"]);
+                IRIS.critical("creating reset code failed",{err,ksn},["api","ise"]);
                 res.json({status: 500, data: {errorCode: "internal_server_error"}})
             })
             .then(resetCode => {
                 if (!["email"].includes(deliveryType)) {
-                    IRIS.warn("incorrect delivery type provided",{deliveryType,ksn},["#api"]);
+                    IRIS.warn("incorrect delivery type provided",{deliveryType,ksn},["api"]);
                     res.json({status: 404, data: {errorCode: "delivery_type_invalid"}});
                 }
                 else {
@@ -220,11 +220,11 @@ exports.requestPasswordResetCode = (req, res) => {
                         postage.sendResetCode(account.email, resetCode, account.fname);
                     }
                     catch(err) {
-                        IRIS.critical("reset code failed to send",{err,ksn},["#api","#catch"]);
+                        IRIS.critical("reset code failed to send",{err,ksn},["api","catch"]);
                         res.json({status: 500, data: {errorCode: "internal_server_error", error: err}});
                     }
                     finally {
-                        IRIS.info("reset code sent perfectly",{ksn},["#api","#success"]);
+                        IRIS.info("reset code sent perfectly",{ksn},["api","success"]);
                         res.json({status: 202, data: {email: account.email}});
                     }
                 }
@@ -238,34 +238,34 @@ exports.verifyResetCode = (req, res) => {
     const code = req.body.reset_code;
     const username = req.body.username;
 
-    IRIS.info("verifying reset code started",{username},["#api"]);
+    IRIS.info("verifying reset code started",{username},["api"]);
 
     ResetCode.findOne({where: {code}})
     .then(resetCode => {
         if (resetCode === null) {
-            IRIS.info("reset code was not found",{resetCode,username},["#api"]);
+            IRIS.info("reset code was not found",{resetCode,username},["api"]);
             res.json({status: 404, data: {errorCode: "reset_code_not_found"}});
         }
         else {
             convertUsernameToKSN(username)
             .catch(err => {
-                IRIS.critical("convert ksn to username failed",{username},["#api","#catch"]);
+                IRIS.critical("convert ksn to username failed",{username},["api","catch"]);
                 res.json({status: 500, data: {errorCode: err.errorCode}})
             })
             .then(ksn => {
                 if (ksn === resetCode.ksn) {
                     ResetCode.destroy({where: {code}})
                     .catch(err => {
-                        IRIS.warn("destroy reset code failed",{code,username,ksn},["#api","#catch"]);
+                        IRIS.warn("destroy reset code failed",{code,username,ksn},["api","catch"]);
                         res.json({status:500, data:{errorCode:"internal_server_error"}});
                     })
                     .then(() => {
-                        IRIS.info("reset code destroyed and verifyResetCode passed",{ksn},["#api","#success"]);
+                        IRIS.info("reset code destroyed and verifyResetCode passed",{ksn},["api","success"]);
                         res.json({status: 202, data: {ksn}});
                     });
                 }
                 else {
-                    IRIS.info("reset code was invalid",{code,ksn},["#api","#else"]);
+                    IRIS.info("reset code was invalid",{code,ksn},["api","else"]);
                     res.json({status: 401, data: {errorCode: "invalid_reset_code"}});
                 }
             });
@@ -280,22 +280,22 @@ exports.resetPassword = (req, res) => {
     const bpassword = bcrypt.hashSync(newPassword, salt);
     const ksn = req.body.ksn;
 
-    IRIS.info("reset password started",{ksn},["#api"]);
+    IRIS.info("reset password started",{ksn},["api"]);
 
     Account.findOne({where: {ksn}})
     .catch(err => {
-        IRIS.critical("Account.findOne failed",{ksn,err},["#api","#ise","#catch"]);
+        IRIS.critical("Account.findOne failed",{ksn,err},["api","ise","catch"]);
         res.json({status: 500, data: {errorCode: "internal_server_error"}});
     })
     .then(account => {
         if (account === null) {
-            IRIS.warn("no account found though there should be",{ksn},["#api"]);
+            IRIS.warn("no account found though there should be",{ksn},["api"]);
             res.json({status: 404, data: {errorCode: "account_not_found"}});
         }
         else {
             Account.update({bpassword}, {where: {ksn}})
             .catch(err => {
-                IRIS.critical("updating password failed",{ksn},["#api","#catch"]);
+                IRIS.critical("updating password failed",{ksn},["api","catch"]);
                 res.json({status: 500, data: {errorCode: "internal_server_error"}});
             })
             .then(update => {
@@ -303,10 +303,10 @@ exports.resetPassword = (req, res) => {
                     postage.sendPasswordResetNotification(account.email, account.fname);
                 }
                 catch (err) {
-                    IRIS.error("send password reset notification failed",{email:account.email,ksn},["#api"]);
+                    IRIS.error("send password reset notification failed",{email:account.email,ksn},["api"]);
                 }
                 finally {
-                    IRIS.info("reset password went perfectly",{update},["#api","#success"]);
+                    IRIS.info("reset password went perfectly",{update},["api","success"]);
                     res.json({status: 202});
                 }
             })
@@ -319,17 +319,17 @@ exports.checkIfCredExists = (req, res) => {
     const type = req.body.type;
     const cred = req.body.cred;
 
-    IRIS.info("check if credential exist starting",{type,cred},["#api"]);
+    IRIS.info("check if credential exist starting",{type,cred},["api"]);
 
     if (type === "username") {
         Account.findOne({where:{username:cred}})
         .then(account => {
             if (account === null) {
-                IRIS.info("credential unexist",{cred,type},["#api"]);
+                IRIS.info("credential unexist",{cred,type},["api"]);
                 res.json({status:202, data:{exists:false}});
             }
             else {
-                IRIS.info("credential exists",{ksn:account.ksn},["#api","#success"]);
+                IRIS.info("credential exists",{ksn:account.ksn},["api","success"]);
                 res.json({status:202, data:{exists:true,fname:account.fname}});
             }
         });
@@ -338,11 +338,11 @@ exports.checkIfCredExists = (req, res) => {
         Account.findOne({where:{email:cred}})
         .then(account => {
             if (account === null) {
-                IRIS.info("credential unexist",{cred,type},["#api"]);
+                IRIS.info("credential unexist",{cred,type},["api"]);
                 res.json({status:202, data:{exists:false}});
             }
             else {
-                IRIS.info("credential exists",{ksn:account.ksn},["#api","#success"]);
+                IRIS.info("credential exists",{ksn:account.ksn},["api","success"]);
                 res.json({status:202, data:{exists:true}});
             }
         });
@@ -351,11 +351,11 @@ exports.checkIfCredExists = (req, res) => {
         Account.findOne({where:{phone_number:cred}})
         .then(account => {
             if (account === null) {
-                IRIS.info("credential unexist",{cred,type},["#api"]);
+                IRIS.info("credential unexist",{cred,type},["api"]);
                 res.json({status:202, data:{exists:false}});
             }
             else {
-                IRIS.info("credential exists",{ksn:account.ksn},["#api","#success"]);
+                IRIS.info("credential exists",{ksn:account.ksn},["api","success"]);
                 res.json({status:202, data:{exists:true}});
             }
         });
@@ -369,19 +369,19 @@ exports.checkIfCredExists = (req, res) => {
 exports.convertEmailToUsername = (req, res) => {
     const email = req.body.email;
 
-    IRIS.info("convert email to username starting",{email},["#api"]);
+    IRIS.info("convert email to username starting",{email},["api"]);
 
     Account.findOne({where:{email}})
     .then(account => {
         if (account === null) {
-            IRIS.info("no account found",{email},["#api"]);
+            IRIS.info("no account found",{email},["api"]);
             res.json({status:404, data:{errorCode:"account_not_found"}});
         }
         else {
             const username = account.username;
             const fname = account.fname;
 
-            IRIS.info("email converting perfectly",{username,fname},["#api","#sucess"]);
+            IRIS.info("email converting perfectly",{username,fname},["api","sucess"]);
             res.json({status:202, data:{username, fname}});
         }
     });
--- controllers/AppchainsController.js
+++ controllers/AppchainsController.js
@@ -3,15 +3,19 @@ const Organization = require("../models/OrganizationModel");
 const generateAIDN = require("../utils/GenerateAIDN");
 const generateACN = require("../utils/GenerateACN");
 const postage = require("../utils/PostageUtils");
+const IRIS = require("../config/iris");
 
 exports.createAppchain = (req, res) => {
     const name = req.body.name;
     const oidn = req.body.oidn;
     const createdat = Date.now();
 
+    IRIS.info("creating appchain started",{name,oidn},["api"]);
+
     Organization.findOne({where: {oidn}})
     .then(org => {
         if (org === null) {
+            IRIS.warn("organization for appchain was not started, though it should",{oidn},["api"]);
             res.json({status: 404, data: {errorCode: "organization_not_found"}});
         }
 
@@ -24,12 +28,19 @@ exports.createAppchain = (req, res) => {
                     oidn,
                     createdat
                 })
-                .catch(err => res.json({status: 500, data: err}))
+                .catch(err => {
+                    IRIS.critical("creating appchain failed",{aidn,acn,oidn},["api","ise"]);
+                    res.json({status: 500, data: err})
+                })
                 .then(appchain => {
                     try {
                         postage.sendNewAppchainCreatedEmail(org.admin_email, name);
                     }
+                    catch (err) {
+                        IRIS.error("sending new appchain created email failed",{aidn},["api"]);
+                    }
                     finally {
+                        IRIS.info("appchain created perfectly",{acn},["api","success"]);
                         res.json({status: 202, data: appchain});   
                     }
                 });
@@ -46,13 +57,15 @@ exports.getAppchains = (req, res) => {};
 exports.deleteAppchain = (req, res) => {
     const aidn = req.body.aidn;
 
+    IRIS.info("deleting appchain started",{aidn},["api"]);
+
     Appchain.destroy({where:{aidn}})
     .catch(err => {
-        console.log(err); //log to iris
+        IRIS.critical("Appchain.destroy failed",{aidn},["api","catch"]);
         res.json({status:500, data:{errorCode: "internal_server_error"}});
     })
     .then(() => {
-        //log to iris
+        IRIS.info("destroyed appchain perfectly",{aidn},["api","success"])''
         res.json({status:202});
     });
 };
