447e9b03be8ed9c8c71b50b69b64d55f9ccb8778
Armaan Gupta <guppythegod@tcff.co>
2020-04-26 12:46:27 -0500

    added iris loggers for service key controllers

--- controllers/ServiceKeyController.js
+++ controllers/ServiceKeyController.js
@@ -2,6 +2,7 @@ const generate = require('nanoid/generate');
 const jwt = require("jsonwebtoken");
 const ServiceKey = require("../models/ServiceKeyModel");
 const generateSKIDN = require("../utils/GenerateSKIDN");
+const IRIS = require("../config/iris");
 
 const rawSecret = process.env.SERVICEKEY_SECURITY_CODE;
 const SECRET = Buffer.from(rawSecret, 'base64');
@@ -15,6 +16,8 @@ exports.createServiceKey = (req, res) => {
     const secureHash = generate("1234567890", 12);
     const service_key = jwt.sign({recieving_aidn, calling_aidn, createdat, secureHash}, SECRET);
 
+    IRIS.info("createServiceKey started",{calling_aidn,recieving_aidn},["api"]);
+
     generateSKIDN(skidn => {
         ServiceKey.create({
             skidn,
@@ -26,11 +29,11 @@ exports.createServiceKey = (req, res) => {
             createdat
         })
         .catch(err => {
-            console.log(err); // log to iris
+            IRIS.critical("ServiceKey.create failed",{skidn,err},["api","catch"]);
             res.json({status:500, data:{errorCode:"internal_server_error"}});
         })
         .then(service_key => {
-            console.log(service_key); // log to iris
+            IRIS.info("service key created perfectly",{skidn},["api","success"]);
             res.json({status:200, data:{service_key}});
         });
     });
@@ -44,21 +47,24 @@ exports.verifyServiceKey = (req, res) => {
     const service_key = req.body.service_key;
     const recieving_aidn = req.body.recieving_aidn;
     const calling_aidn = req.body.calling_aidn;
+
+    IRIS.info("verifyServiceKey started",{service_key,recieving_aidn,calling_aidn},["api"]);
     
     jwt.verify(service_key, SECRET, (err, payload) => {
         if (err) {
-            console.log("invalid_service_key"); // log to iris
+            IRIS.info("service key was invalid",{service_key},["api"]);
             res.json({status:401, data:{errorCode:"invalid_service_key"}});
         }
         else if (payload.recieving_aidn !== recieving_aidn) {
-            console.log("recieving_aidn_mismatch"); // log to iris
+            IRIS.info("recieving_aidn mismatch",{service_key},["api"]);
             res.json({status:401, data:{errorCode:"recieving_aidn_mismatch"}});
         }
         else if (payload.calling_aidn !== calling_aidn) {
-            console.log("calling_aidn_mismatch"); // log to iris
+            IRIS.info("calling_aidn mismatch",{service_key},["api"]);
             res.json({status:401, data:{errorCode:"calling_aidn_mismatch"}});
         }
         else {
+            IRIS.info("service key verification passed",{service_key},["api","success"]);
             res.json({status:202, data:{statusCode:"verification_passed"}});
         }
     });
@@ -68,13 +74,15 @@ exports.verifyServiceKey = (req, res) => {
 exports.deleteServiceKey = (req, res) => {
     const skidn = req.body.skidn;
 
+    IRIS.info("deleteServiceKey started",{skidn},["api"]);
+
     ServiceKey.destroy({where:{skidn}})
     .catch(err => {
-        console.log(err); // log to iris
+        IRIS.critical("ServiceKey.destroy failed",{skidn},["api","ise"]);
         res.json({status:500, data:{errorCode:"internal_server_error"}});
     })
     .then(() => {
-        //log to iris
+        IRIS.info("destroyed service key perfectly",{skidn},["api","success"]);
         res.json({status:202});
     })
 }
