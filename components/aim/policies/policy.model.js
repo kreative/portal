8aa23846966908e334be7cc5dd0b662635934a6a
Armaan Gupta <guppythegod@tcff.co>
2020-05-05 12:37:36 -0500

    Created policy model and validatePolicies util

--- components/aim/policies/policy.model.js
+++ components/aim/policies/policy.model.js
@@ -2,7 +2,26 @@ const Sequelize = require("sequelize");
 const DB = require("../../../config/db").sequelize;
 
 const Policy = DB.define('policy', {
-
+    policy: {
+        type: Sequelize.TEXT,
+        primaryKey: true,
+    },
+    associate_appchain: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        references: {
+            model: 'appchain',
+            key: 'acn'
+        }
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+    },
+    createdat: {
+        type: Sequelize.BIGINT,
+        allowNull: false
+    }
 }, {
     timestamps: false
 });
--- utils/ValidatePolicies.js
+++ utils/ValidatePolicies.js
@@ -1,8 +1,20 @@
-const Policies = require("../components/aim/policies/policy.model");
+const Policy = require("../components/aim/policies/policy.model");
+
+const validatePolicy = (_policy) => {
+    return new Promise((resolve, reject) => {
+        Policy.findOne({where:{policy:_policy}})
+        .then(policy => {
+            if (policy === null) reject(`policy_not_found: ${policy}`);
+            else resolve();
+        });
+    });
+};
 
 const validatePolicies = (policies) => {
     return new Promise((resolve, reject) =>{
-        resolve();
+        Promise.all(policies.map(policy => validatePolicy(policy)))
+        .catch(err => reject(err))
+        .then(vals => resolve(202));
     })
 };
 
