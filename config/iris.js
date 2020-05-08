bded756e0c1f83fcbfb8bda3e8c6a406af595321
Armaan Gupta <guppythegod@tcff.co>
2020-05-08 12:11:46 -0500

    removed dockerfile among other things

--- config/iris.js
+++ config/iris.js
@@ -1,8 +1,78 @@
-const Iris = require("@kaynet/iris-node-sdk");
+const axios = require("axios");
 
-const SERVICE_KEY = process.env.PORTAL_TO_IRIS_SK;
+class Iris {
+
+    constructor(aidn, irisUrl) {
+        this.aidn = parseInt(aidn);
+        this.url = irisUrl + "/log";
+    }
+
+    debug(message, meta, tags) {
+        axios.post(this.url, {
+            message,
+            tags,
+            meta,
+            level: 'debug',
+            aidn: this.aidn
+        })
+        .catch(error => {return error})
+        .then(response => {return response});
+    };
+
+    info(message, meta, tags) {
+        axios.post(this.url, {
+            message,
+            tags,
+            meta,
+            level: 'info',
+            aidn: this.aidn
+        })
+        .catch(error => {return error})
+        .then(response => {return response});
+    };
+
+    error(message, meta, tags) {
+        axios.post(this.url, {
+            message,
+            tags,
+            meta,
+            level: 'error',
+            aidn: this.aidn
+        })
+        .catch(error => {
+            console.log(error);
+            return error
+        })
+        .then(response => {return response});
+    };
+
+    warn(message, meta, tags) {
+        axios.post(this.url, {
+            message,
+            tags,
+            meta,
+            level: 'warn',
+            aidn: this.aidn
+        })
+        .catch(error => {return error})
+        .then(response => {return response});
+    };
+
+    critical(message, meta, tags) {
+        axios.post(this.url, {
+            message,
+            tags,
+            meta,
+            level: 'critical',
+            aidn: this.aidn
+        })
+        .catch(error => {return error})
+        .then(response => {return response});
+    };
+}
+
 const AIDN = process.env.AIDN;
 
-const IRIS = new Iris(AIDN, SERVICE_KEY);
+const IRIS = new Iris(AIDN, "https://iris.kreative.im");
 
 module.exports = IRIS;
