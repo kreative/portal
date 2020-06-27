339a9625e91a405f39c21ca0d138492783e25879
Armaan Gupta <armaan@kreative.im>
2020-06-27 10:44:37 -0500

    Removed verifyKey from organizations api

--- components/organizations/routes.js
+++ components/organizations/routes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const verifyKey = require("../../middleware/VerifyKeyMiddleware");
 const organizations = require("./organization.controller");
 
-router.get('/api/organizations', verifyKey, organizations.getOrganizations);
-router.post('/api/organizations', verifyKey, organizations.createOrganization);
+// router.get('/api/organizations', verifyKey, organizations.getOrganizations);
+// router.post('/api/organizations', verifyKey, organizations.createOrganization);
+
+router.get('/api/organizations', organizations.getOrganizations);
+router.post('/api/organizations', organizations.createOrganization);
 
 module.exports = router;
