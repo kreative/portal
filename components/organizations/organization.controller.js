c4be3f20a35faf4a830dd05458cb569dc60baaaa
Armaan Gupta <armaan@kreative.im>
2020-06-29 09:09:53 -0500

    Added removeOrganization method

--- components/organizations/organization.controller.js
+++ components/organizations/organization.controller.js
@@ -4,54 +4,69 @@ const postage = require("../../lib/postage/postage.utils");
 const IRIS = require("../../config/iris");
 
 exports.createOrganization = (req, res) => {
-    const ksn = req.headers['portal_ksn'];
-    const name = req.body.name;
-    const admin_email = req.body.admin_email;
-    const createdat = Date.now();
+  const ksn = req.headers["portal_ksn"];
+  const name = req.body.name;
+  const admin_email = req.body.admin_email;
+  const createdat = Date.now();
 
-    IRIS.info("creating organization started",{ksn,admin_email},["api"]);
+  IRIS.info("creating organization started", { ksn, admin_email }, ["api"]);
 
-    generate.oidn(oidn => {
-        Organization.create({
-            oidn,
-            ksn,
-            name,
-            admin_email,
-            createdat
-        })
-        .catch(err => {
-            IRIS.critical("Organization.create failed",{ksn,admin_email},["api","ise"]);
-            res.json({status: 500, data: err})
-        })
-        .then(org => {
-            try {
-                postage.sendNewOrganizationCreatedEmail(admin_email, name);
-            }
-            catch (err) {
-                IRIS.error("sendNewOrganizationCreatedEmail failed",{admin_email,oidn},["api","catch"]);
-            }
-            finally {
-                IRIS.info("organization created perfectly",{oidn},["api","success"]);
-                res.json({status: 202, data: org});
-            }
-        });
-    });
+  generate.oidn((oidn) => {
+    Organization.create({
+      oidn,
+      ksn,
+      name,
+      admin_email,
+      createdat,
+    })
+      .catch((err) => {
+        IRIS.critical("Organization.create failed", { ksn, admin_email }, [
+          "api",
+          "ise",
+        ]);
+        res.json({ status: 500, data: err });
+      })
+      .then((org) => {
+        try {
+          postage.sendNewOrganizationCreatedEmail(admin_email, name);
+        } catch (err) {
+          IRIS.error(
+            "sendNewOrganizationCreatedEmail failed",
+            { admin_email, oidn },
+            ["api", "catch"]
+          );
+        } finally {
+          IRIS.info("organization created perfectly", { oidn }, [
+            "api",
+            "success",
+          ]);
+          res.json({ status: 202, data: org });
+        }
+      });
+  });
 };
 
 // returns list of organizations belonging to a KSN
 // but for now it just returns all the organizations
 exports.getOrganizations = (req, res) => {
-    Organization.findAll().then(organizations => {
-        res.json({status:202, data:{organizations}});
+  Organization.findAll()
+    .then((organizations) => {
+      res.json({ status: 202, data: { organizations } });
     })
-    .catch(error => {
-        IRIS.critical("getOrganizations failed", {error}, ["api", "ise"]);
-        res.json({status:500, data:{errorCode:"internal_server_error"}});
+    .catch((error) => {
+      IRIS.critical("getOrganizations failed", { error }, ["api", "ise"]);
+      res.json({ status: 500, data: { errorCode: "internal_server_error" } });
     });
 };
 
-
 exports.updateOrganization = (req, res) => {};
 
+exports.removeOrganization = (req, res) => {
+  const oidn = req.body.oidn;
 
-exports.removeOrganization = (req, res) => {};
+  Organization.destroy({ where: { oidn } })
+    .then(() => res.json({ status: 202 }))
+    .catch((error) => {
+      res.json({ status: 500, data: { errorCode: "internal_server_error" } });
+    });
+};
