09be606e91921adfc94c60e210954b512feea7ae
Armaan Gupta <armaan@kreative.im>
2020-06-29 19:03:30 -0500

    Removed verifyKey from appchain routes

--- components/appchains/routes.js
+++ components/appchains/routes.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const verifyKey = require("../../middleware/VerifyKeyMiddleware");
 const appchains = require('./appchain.controller');
 
-router.post('/api/appchains', verifyKey, appchains.createAppchain);
-router.delete('/api/appchains', verifyKey, appchains.deleteAppchain);
+// NEED TO ADD VERIFY KEY TO THESE ROUTES
+
+router.post('/api/appchains', appchains.createAppchain);
+router.delete('/api/appchains', appchains.deleteAppchain);
 
 module.exports = router;
