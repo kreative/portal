7232e2c17b29445592771660297d23899bbca74f
Armaan Gupta <armaan@kreative.im>
2020-07-02 11:34:29 -0500

    Reversed destroy order for deletePermit

--- utils/DeletePermits.js
+++ utils/DeletePermits.js
@@ -4,12 +4,12 @@ const deleteWarrants = require("./DeleteWarrants");
 const deletePermit = (permit) => {
   return new Promise((resolve, reject) => {
     const permit_id = permit.permit_id;
-    Permit.destroy({ where: { permit_id } })
+    deleteWarrants.fromPermitID(permit_id)
       .catch((error) => reject(error))
-      .then(() => {
-        deleteWarrants.fromPermitID(permit_id)
+      .then((values) => {
+        Permit.destroy({ where: { permit_id } })
           .catch((error) => reject(error))
-          .then((values) => resolve(202));
+          .then(() => resolve(202));
       });
   });
 };
