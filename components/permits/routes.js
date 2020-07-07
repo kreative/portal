ef899dbde8a92d7620c28593ae9fae79efad57cc
Armaan Gupta <guppythegod@kreative.im>
2020-07-07 11:10:28 -0500

    Rewrote routes for permits api

--- components/permits/routes.js
+++ components/permits/routes.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const verifyKey = require("../../middleware/VerifyKeyMiddleware");
 const permits = require("./permit.controller");
 
-router.get('/api/permits/dev', verifyKey, permits.getPermitsForDev);
-router.get('/api/permits/app', verifyKey, permits.getPermitsForDev);
-router.post('/api/permits', verifyKey, permits.createPermit);
-router.put('/api/permits', verifyKey, permits.updatePermit);
-router.put('/api/permits/deactivate', verifyKey, permits.deactivatePermit);
-router.delete('/api/permits', verifyKey, permits.deletePermit);
+// need to add verifyKey eventually
+router.get('/api/permits', permits.getPermits);
+router.post('/api/permits', permits.createPermit);
+router.put('/api/permits/:permit_id', permits.updatePermit);
+router.put('/api/permits/deactivate/:permit_id', permits.deactivatePermit);
+router.delete('/api/permits/:permit_id', permits.deletePermit);
 
 module.exports = router;
