d6a98248cc9e9f52b8a487087aa9d64a92388162
Armaan Gupta <guppythegod@kreative.im>
2020-08-16 18:05:36 -0500

    Removed IRIS and added keychain verification through expire_by date

--- utils/VerifyKey.js
+++ utils/VerifyKey.js
@@ -1,6 +1,5 @@
 const Keychain = require('../components/accounts/keychain.model');
 const jwt = require('jsonwebtoken');
-const IRIS = require("../config/iris");
 
 const rawSecret = process.env.KEYCHAIN_SECURITY_CODE;
 const SECRET = Buffer.from(rawSecret, 'base64');
@@ -8,36 +7,23 @@ const SECRET = Buffer.from(rawSecret, 'base64');
 const verifyKey = (key, ksn, aidn) => {
     return new Promise((resolve, reject) => {
         jwt.verify(key, SECRET, (err, payload) => {
-            if (err) {
-                IRIS.info("key invalid",{ksn,aidn,key},["api"]);
-                reject({status:401, code:"invalid_key"});
-            }
-
-            if (payload.ksn !== ksn) {
-                IRIS.info("ksn mismatch",{ksn,aidn,key},["api"]);
-                reject({status:401, code:"ksn_mismatch"});
-            }
-
-            if (payload.aidn !== aidn) {
-                IRIS.info("aidn mismatch",{ksn,aidn,key},["api"]);
-                reject({status:401, code:"aidn_mismatch"});
-            }
+            if (err) reject({status:401, code:"invalid_key"});
+            if (payload.ksn !== ksn) reject({status:401, code:"ksn_mismatch"});
+            if (payload.aidn !== aidn) reject({status:401, code:"aidn_mismatch"});
 
             Keychain.findOne({where: {key}})
-            .catch(err => {
-                IRIS.critical("Keychain.findOne failed",{key,err},["api","ise"]);
-                reject({status:500, code:"internal_server_error"});
-            })
-            .then(keychain => {
-                if (keychain.expired) {
-                    IRIS.info("keychain was expired",{key},["api"]);
-                    reject({status:401, code:"expired_keychain"});
-                }
-                
-                const ccn = keychain.ccn;
-
-                resolve({ksn, ccn});
-            });
+                .catch(err => {
+                    reject({status:500, code:"internal_server_error"});
+                })
+                .then(keychain => {
+                    if (keychain.expired || ((Date.now() - keychain.expire_by)/1000/60 > 0)) {
+                        reject({status:401, code:"expired_keychain"});
+                    }
+                    else {
+                        const ccn = keychain.ccn;
+                        resolve({ksn, ccn});
+                    }
+                });
         });
     });
 };
